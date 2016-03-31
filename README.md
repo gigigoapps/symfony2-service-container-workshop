3f15a010a81454ff68816267e905e45a88a5ae89
Blas Castellano Moreno <blas.castellano@gigigo.com>
2016-04-01 01:40:25 +0200

    Create example for simple service

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # symfony2-service-container-workshop
 A workshop to introduce symfony 2 service container
+
+# Branch simple-service
+Create a simple service crypto to see how it works
+ - Check debug
+ - See appDevDebugProjectContainer.php file to see php generated
--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -5,6 +5,7 @@ namespace AppBundle\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\VarDumper\VarDumper;
 
 class DefaultController extends Controller
 {
@@ -13,6 +14,8 @@ class DefaultController extends Controller
      */
     public function indexAction(Request $request)
     {
+        VarDumper::dump($this->get('core.crypto'));
+
         // replace this example code with whatever you need
         return $this->render('default/index.html.twig', array(
             'base_dir' => realpath($this->container->getParameter('kernel.root_dir').'/..'),
